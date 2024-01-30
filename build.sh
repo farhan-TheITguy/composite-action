f608d8196987a8f4bc3c02258557131f6e30138b
Mohd Farhan <85340728+farhan-TheITguy@users.noreply.github.com>
2024-01-30 19:18:41 +0530

    update

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Building image with version $1"
+sleep 2
+echo "Image build completed - $1"
---
+++ push.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Pushing the image with tag $1"
+sleep 2
+echo "Image push completed - $1"
